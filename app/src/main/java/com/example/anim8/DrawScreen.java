9f2728a65344d0dc728fc407f276321304ecbc4a
maroj44 <jakejam44@sbcglobal.net>
2020-06-11 11:16:16 -0500

    drawscreen

--- app/src/main/java/com/example/anim8/DrawScreen.java
+++ app/src/main/java/com/example/anim8/DrawScreen.java
@@ -1,4 +1,124 @@
 package com.example.anim8;
 
-public class DrawScreen {
+import java.util.ArrayList;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class DrawScreen extends View implements OnTouchListener {
+    private Canvas drawCanvas;
+    private Path drawPath;
+    private Paint drawPaint;
+    private ArrayList<Path> paths = new ArrayList<Path>();
+    private ArrayList<Path> undonePaths = new ArrayList<Path>();
+
+    private Bitmap bitmap;
+    public DrawScreen(Context context)
+    {
+        super(context);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setOnTouchListener(this);
+        drawPaint = new Paint();
+        drawPaint.setAntiAlias(true);
+        drawPaint.setDither(true);
+        drawPaint.setColor(0xFFFFFFFF);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPaint.setStrokeWidth(6);
+        drawCanvas = new Canvas();
+        drawPath = new Path();
+
+        bitmap = BitmapFactory.decodeResource(context.getResources(),R.mipmap.ic_launcher); // was just drawable.ic_launcher
+
+
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //drawPath = new Path();
+        //canvas.drawPath(drawPath, drawPaint);
+        for (Path p : paths){
+            canvas.drawPath(p, drawPaint);
+        }
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    private float mX, mY;
+    private static final float TOUCH_TOLERANCE = 4;
+
+    private void touch_start(float x, float y) {
+        undonePaths.clear();
+        drawPath.reset();
+        drawPath.moveTo(x, y);
+        mX = x;
+        mY = y;
+    }
+    private void touch_move(float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+        if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            drawPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+            mX = x;
+            mY = y;
+        }
+    }
+    private void touch_up() {
+        drawPath.lineTo(mX, mY);
+        // commit the path to our offscreen
+        drawCanvas.drawPath(drawPath, drawPaint);
+        // kill this so we don't double draw
+        paths.add(drawPath);
+        drawPath = new Path();
+
+    }
+
+    public void onClickUndo () {
+        if (paths.size()>0)
+        {
+            undonePaths.add(paths.remove(paths.size()-1));
+            invalidate();
+        }
+    }
+
+    public void onClickRedo (){
+        if (undonePaths.size()>0)
+        {
+            paths.add(undonePaths.remove(undonePaths.size()-1));
+            invalidate();
+        }
+    }
+
+    @Override
+    public boolean onTouch(View arg0, MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touch_start(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touch_move(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touch_up();
+                invalidate();
+                break;
+        }
+        return true;
+    }
 }
